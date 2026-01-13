769d41a46392290083c5ff40452d15221a297202
RiRith69 <chrensophearith810@gmail.com>
2026-01-13 08:54:14 +0700

    First commit

---
+++ Frontend/src/utils/SignLabel.js
@@ -0,0 +1,24 @@
+const signLabels = {
+  stop: {
+    name: "Stop Sign",
+    description: "Indicates that drivers must stop completely.",
+    image: "/assets/signs/stop.png"
+  },
+  speed_limit_50: {
+    name: "Speed Limit 50",
+    description: "Maximum speed allowed is 50 km/h.",
+    image: "/assets/signs/speed_limit_50.png"
+  },
+  no_entry: {
+    name: "No Entry",
+    description: "Vehicles must not enter this road.",
+    image: "/assets/signs/no_entry.png"
+  },
+  yield: {
+    name: "Yield",
+    description: "Drivers must give way to other vehicles.",
+    image: "/assets/signs/yield.png"
+  }
+};
+
+export default signLabels;
